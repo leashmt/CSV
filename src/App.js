eab04ec45e2da778b226a21815f70d6800b69580
leaschmt <lea.schmitt@supdevinci-edu.fr>
2024-12-26 13:46:10 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+	return (
+		<div className="App">
+			<div className="text-center text-2xl text-blue-500">
+				Tailwind fonctionne !
+			</div>
+		</div>
+	);
+}
+
+export default App;
